b3e540826fed538ab16ee739a4c873082528e413
leftzio <left.zioris@gmail.com>
2023-12-11 21:28:19 +0200

    styles

--- angular-app/src/app/catalogue/catalogue.component.ts
+++ angular-app/src/app/catalogue/catalogue.component.ts
@@ -16,65 +16,66 @@ export class CatalogueComponent implements OnInit {
 
   sectionRecipes = []
 
-  recipes: Recipes[] = [
-    {
-      title: 'Recipe 1',
-      instructions: [
-        { key: 'step1' , value:[ 'Step 1 instruction'] },
-        { key: 'step2', value:[ 'Step 2 instruction'] },
-        // Add more steps as needed
-      ],
-      ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-      equipment: ['Equipment 1', 'Equipment 2'],
-      tags: ['Tag 1', 'Tag 2'],
-      photos_urls: '../../assets/taper.jpg',
-      preperation_time: 30,
-      cooking_time: 60,
-      difficulty: 'Intermediate',
-      rating: {
-        sum: 40,
-        counter: 10
-      }
-    },
-    {
-      title: 'Recipe 2',
-      instructions: [
-        { key: 'step1' , value:[ 'Step 11 instruction'] },
-        { key: 'step2', value:[ 'Step 22 instruction'] },
-        // Add more steps as needed
-      ],
-      ingredients: ['Ingredient A', 'Ingredient B', 'Ingredient C'],
-      equipment: ['Equipment X', 'Equipment Y'],
-      tags: ['Tag X', 'Tag Y'],
-      photos_urls: '../../assets/taper.jpg',
-      preperation_time: 45,
-      cooking_time: 75,
-      difficulty: 'Advanced',
-      rating: {
-        sum: 32,
-        counter: 10
-      }
-    },
-    {
-      title: 'Recipe 3',
-      instructions: [
-        { key: 'step1' , value:[ 'Step 111 instruction'] },
-        { key: 'step2', value:[ 'Step 222 instruction'] },
-        // Add more steps as needed
-      ],
-      ingredients: ['Ingredient X', 'Ingredient Y', 'Ingredient Z'],
-      equipment: ['Equipment A', 'Equipment B'],
-      tags: ['Tag A', 'Tag B'],
-      photos_urls: '../../assets/taper.jpg',
-      preperation_time: 60,
-      cooking_time: 90,
-      difficulty: 'Beginner',
-      rating: {
-        sum: 25,
-        counter: 10
-      }
-    },
-  ];; // Assuming your recipe objects have a structure, adjust accordingly
+  public recipes: Recipes[] = []
+  // recipes: Recipes[] = [
+  //   {
+  //     title: 'Recipe 1',
+  //     instructions: [
+  //       { key: 'step1' , value:[ 'Step 1 instruction'] },
+  //       { key: 'step2', value:[ 'Step 2 instruction'] },
+  //       // Add more steps as needed
+  //     ],
+  //     ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
+  //     equipment: ['Equipment 1', 'Equipment 2'],
+  //     tags: ['Tag 1', 'Tag 2'],
+  //     photo_url: '../../assets/taper.jpg',
+  //     preperation_time: 30,
+  //     cooking_time: 60,
+  //     difficulty: 'Intermediate',
+  //     rating: {
+  //       sum: 40,
+  //       counter: 10
+  //     }
+  //   },
+  //   {
+  //     title: 'Recipe 2',
+  //     instructions: [
+  //       { key: 'step1' , value:[ 'Step 11 instruction'] },
+  //       { key: 'step2', value:[ 'Step 22 instruction'] },
+  //       // Add more steps as needed
+  //     ],
+  //     ingredients: ['Ingredient A', 'Ingredient B', 'Ingredient C'],
+  //     equipment: ['Equipment X', 'Equipment Y'],
+  //     tags: ['Tag X', 'Tag Y'],
+  //     photo_url: '../../assets/taper.jpg',
+  //     preperation_time: 45,
+  //     cooking_time: 75,
+  //     difficulty: 'Advanced',
+  //     rating: {
+  //       sum: 32,
+  //       counter: 10
+  //     }
+  //   },
+  //   {
+  //     title: 'Recipe 3',
+  //     instructions: [
+  //       { key: 'step1' , value:[ 'Step 111 instruction'] },
+  //       { key: 'step2', value:[ 'Step 222 instruction'] },
+  //       // Add more steps as needed
+  //     ],
+  //     ingredients: ['Ingredient X', 'Ingredient Y', 'Ingredient Z'],
+  //     equipment: ['Equipment A', 'Equipment B'],
+  //     tags: ['Tag A', 'Tag B'],
+  //     photo_url: '../../assets/taper.jpg',
+  //     preperation_time: 60,
+  //     cooking_time: 90,
+  //     difficulty: 'Beginner',
+  //     rating: {
+  //       sum: 25,
+  //       counter: 10
+  //     }
+  //   },
+  // ];; // Assuming your recipe objects have a structure, adjust accordingly
 
 
 constructor(private recipeService:RecipesService) {
@@ -88,6 +89,8 @@ constructor(private recipeService:RecipesService) {
   }
 
   ngOnInit() {
+
+    console.log(' my recipes',this.myRecipes)
     // Make an HTTP call to fetch recipes from the server
     if(this.previousRoute == "home"){
       this.recipeService.getRecipes().subscribe(
